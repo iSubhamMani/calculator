de08cbb446cba9b275303ace1370b775ae834bfb
Subham Mani <manisubham09@gmail.com>
2023-07-11 22:11:45 +0530

    fix some bugs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let display = document.querySelector('.display');
 let output = document.querySelector('.output');
 const nums = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
+const dangerBtns = document.querySelectorAll('.danger');
 const equalBtn = document.querySelector('.btn-equals');
 
 const operatorsList = ["+", "-", "*", "/", "%"];
@@ -9,22 +10,49 @@ const operatorsList = ["+", "-", "*", "/", "%"];
 let operatorKey = "";
 let firstNumber = "";
 let secondNumber = "";
-let isOperatorUsed = false;
+let operatorUsed = false;
 let result = 0;
+let dotUsed = false;
+
+window.addEventListener('keydown', (e) => {
+    if(e.code === "Backspace") deleteLast();
+})
+
+dangerBtns.forEach(dangerBtn => {
+    dangerBtn.addEventListener('click', (e) => {
+    
+        if(e.target.dataset.value === "AC"){
+            //clear all
+            clearAll();
+        }
+        else if(e.target.dataset.value === "DEL"){
+            //delete one char
+            deleteLast();
+        }
+    })
+})
 
 nums.forEach(num => {
     num.addEventListener('click', (e) => {
+        if(e.target.dataset.value === "." && !dotUsed) dotUsed = true;
+        else if(e.target.dataset.value === "." && dotUsed) return;
         display.value += e.target.dataset.value;
     })
 })
 
 operators.forEach(operator => {
     operator.addEventListener('click', (e) => {
-        if(isOperatorUsed){
+        if(operatorUsed){
+            dotUsed = false;
             secondNumber = display.value;
             if(secondNumber === "") return;
-            else {
-                result = calculate(Number(firstNumber), Number(secondNumber), operatorKey);
+            if(secondNumber === "0") {
+                output.value = "Can't divide by zero";
+                secondNumber="";
+                return;
+            }
+            else { 
+                result = +(Math.round(calculate(Number(firstNumber), Number(secondNumber), operatorKey) + "e+2")  + "e-2");
                 display.value = "";
                 output.value = result;
                 firstNumber = result;
@@ -32,29 +60,36 @@ operators.forEach(operator => {
             }
         }
 
-        if(!isOperatorUsed){
+        if(!operatorUsed){
+            dotUsed = false;
             firstNumber = display.value;
             if(firstNumber === "") return;
             operatorKey = e.target.dataset.value;
             display.value = "";
-            isOperatorUsed = true;
+            operatorUsed = true;
         }
     })
 })
 
 equalBtn.addEventListener('click', (e) => {
-    if(isOperatorUsed){
+    if(operatorUsed){
         secondNumber = display.value;
         if(secondNumber === "") return;
+        if(secondNumber === "0"){
+            output.value = "Can't divide by zero";
+            secondNumber="";
+            return;
+        }
         
         displayResult();
     }
 })
 
 function displayResult(){
-    result = calculate(Number(firstNumber), Number(secondNumber), operatorKey);
+    result = +(Math.round(calculate(Number(firstNumber), Number(secondNumber), operatorKey) + "e+2")  + "e-2");
     output.value = result;
-    isOperatorUsed = false;
+    operatorUsed = false;
+    dotUsed = false;
 }
 
 function calculate(firstNumber, secondNumber, operator){
@@ -63,4 +98,20 @@ function calculate(firstNumber, secondNumber, operator){
     else if(operator === "*") return (firstNumber * secondNumber);
     else if(operator === "/") return (firstNumber / secondNumber);
     else if(operator === "%") return (firstNumber % secondNumber);
+}
+
+function deleteLast(){
+    const txt = display.value;
+    display.value = txt.slice(0, -1);
+}
+
+function clearAll(){
+    display.value = "";
+    output.value = "";
+    operatorKey = "";
+    operatorUsed = false;
+    dotUsed = false;
+    firstNumber = "";
+    secondNumber = "";
+    result = 0;
 }
